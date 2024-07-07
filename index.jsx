2c53e57cc8d55839e9466d3f5a21efa4a9743483
Rakesh Kundu <kunrak01@csgicorp.com>
2024-07-08 01:34:36 +0530

    changes to the main branch

--- index.jsx
+++ index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import App from "./App";
 
 function index() {
   return (
-    <div>index</div>
-  )
+    <div>
+      <App />
+    </div>
+  );
 }
 
-export default index
+export default index;
